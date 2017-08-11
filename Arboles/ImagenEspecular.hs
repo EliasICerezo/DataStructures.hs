3f8365ed2eb0fa7c24f070554a7b2048c48a1e32
MagicElyas <elias2603@hotmail.com>
2017-08-11 17:30:10 +0200

    Termino Issymetric.hs
    Termino la implementacion de la funcion que devuelve verdadero si un arbol es simetrico

--- Arboles/ImagenEspecular.hs
+++ Arboles/ImagenEspecular.hs
@@ -6,3 +6,14 @@ data TreeB a = EmptyB | NodeB a (TreeB a) (TreeB a) deriving Show
 mirror::TreeB a->TreeB a
 mirror EmptyB = EmptyB
 mirror (NodeB x lt rt) = (NodeB x (mirror rt) (mirror lt))
+
+isSymetricB::(Eq a)=> TreeB a-> Bool
+isSymetricB (NodeB _ lt rt) = simetricos rt (mirror lt)
+
+simetricos::(Eq a)=>TreeB a->TreeB a->Bool
+simetricos EmptyB EmptyB =True
+simetricos EmptyB y = False
+simetricos x EmptyB = False
+simetricos (NodeB x lt rt) (NodeB y ly ry)
+                  |x==y = (simetricos lt ly) && (simetricos rt ry)
+                  |otherwise = False
